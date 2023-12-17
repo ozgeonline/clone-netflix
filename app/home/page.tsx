6b54902f450eff15829bafc0f479809517b0e205
ozgeonline <nisroc.414@hotmail.com>
2023-12-18 01:08:04 +0300

    pages

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,56 +1,12 @@
-import { Button } from "@/components/ui/button";
-import prisma from "../utils/db";
-// import MovieButtons from "./MovieButtons";
+import MovieVideo from "../components/MovieVideo";
+import RecentlyAdded from "../components/RecentlyAdded";
 
-async function getData() {
-  const data = await prisma.movie.findFirst({
-    select: {
-      title: true,
-      overview: true,
-      videoSource: true,
-      imageString: true,
-      release: true,
-      duration: true,
-      id: true,
-      age: true,
-      youtubeString: true,
-    },
-  });
-  return data
-}
-
-export default async function MovieVideo() {
-  const data = await getData();
-
+export default function HomePage() {
   return (
-    <div className="h-[55vh] lg:h-[60vh] w-full flex justify-start items-center">
-      <video
-        poster={data?.imageString}
-        autoPlay
-        muted
-        loop
-        src={data?.videoSource}
-        className="w-full absolute top-0 left-0 h-[60vh] object-cover -z-10 brightness-[60%]"
-      ></video>
-
-      <div className="absolute w-[90%] lg:w-[40%] mx-auto">
-        <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold">
-          {data?.title}
-        </h1>
-        <p className="text-white text-lg mt-5 line-clamp-3">{data?.overview}</p>
-        {/* <div className="flex gap-x-3 mt-4">
-          <MovieButtons
-            age={data?.age as number}
-            duration={data?.duration as number}
-            id={data?.id as number}
-            overview={data?.overview as string}
-            releaseDate={data?.release as number}
-            title={data?.title as string}
-            youtubeUrl={data?.youtubeString as string}
-            key={data?.id}
-          />
-        </div> */}
-      </div>
+    <div className="p-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-3xl font-bold ">Recently Added</h1>
+      <RecentlyAdded />
     </div>
   );
 }
